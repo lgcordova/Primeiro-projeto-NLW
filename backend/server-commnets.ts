1021b2b05110cefb7c6965e5e15c350c9d3c32f2
lgcordova <luizgcordova.barbosa@gmail.com>
2026-03-07 23:01:10 -0300

    primeiro commit

---
+++ backend/server-commnets.ts
@@ -0,0 +1,30 @@
+// import express, { request, response } from "express";
+
+// const app = express();
+
+// app.get("/test", (request, response) => {
+//   // request => tudo que entra
+//   // response => tudo que sai 
+//   return response.send("olá NLW")
+// })
+
+// /** GET = Busca uma informação  
+//  *  POST = Inserir (Criar) uma informação
+//  *  PUT = Alterar uma informação
+// *   DELETE = Remover um dado
+// *   PATCH = Alterar uma informação especifica
+// */
+
+// /** Tipos de paremetros
+//  *  routes params = http://localhost:3000/produtos/6468432168
+//  * query params = http://localhost:3000/produtos?Nome=teclado&description=tecladobom7&...
+//  *  Body Params => {
+//  *  "nome": teclado
+//  *  "description": "tecladobom" } 
+//  */
+
+// app.post("/teste-post", (request, response) => {
+//   return response.send("olá Nlw new")
+// })  
+
+// app.listen(3000, () => console.log("server is running") ) 
